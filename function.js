64e9938e409790445379897b3e5a93911f3a1109
Antorkarmokar28 <antorkarmokar28@gmail.com>
2025-11-08 21:01:33 +0600

    functional problem solved

---
+++ function.js
@@ -0,0 +1,9 @@
+// Task-1
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiplyNumber(num1, num2, num3, num4) {
+  return num1 * num2 * num3 * num4;
+}
+
+const result = multiplyNumber(2, 4, 6, 2);
+console.log(result);
