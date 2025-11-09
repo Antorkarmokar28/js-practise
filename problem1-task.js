24d902a97c5a848215196860131bb7e3d6f963ae
Antorkarmokar28 <antorkarmokar28@gmail.com>
2025-11-09 20:22:38 +0600

    Temperature celsius to farhenheit conert problem solved

---
+++ problem1-task.js
@@ -0,0 +1,7 @@
+// Task-1:
+// Write a function to convert temperature from Celsius to Fahrenheit.
+function temperatureToFarenheit(celsius) {
+  return celsius * (9 / 5) + 32;
+}
+const output = temperatureToFarenheit(35);
+console.log(output);
