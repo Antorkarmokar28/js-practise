7998413798476fa0ac6156913220d95eea0361a6
Antorkarmokar28 <antorkarmokar28@gmail.com>
2025-11-11 21:37:36 +0600

    Smallest name find out related problem solved

--- problem.js
+++ problem.js
@@ -11,6 +11,23 @@ function lowestNumber(numbers) {
   }
   return lowest;
 }
-const heights2 = [167, 190, 120, 165, 137];
-const output = lowestNumber(heights2);
+const numbers = [167, 190, 120, 165, 137];
+const output = lowestNumber(numbers);
 console.log(output);
+// Task -2:
+// Find the friend with the smallest name.
+// const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+function smallestName(names) {
+  let smallest = names[0];
+  for (let i = 0; i < names.length; i++) {
+    const name = names[i];
+    if (name.length < smallest.length) {
+      smallest = name;
+    }
+  }
+  return smallest;
+}
+
+const names = ["rahim", "robin", "rafi", "ron", "rashed"];
+const output1 = smallestName(names);
+console.log(output1);
