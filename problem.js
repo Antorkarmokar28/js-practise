eb51c99f02a6358fe74364f0255722c09ce5bdc5
Antorkarmokar28 <antorkarmokar28@gmail.com>
2025-11-11 21:47:08 +0600

    Total calculate electronics budget problem solved

--- problem.js
+++ problem.js
@@ -31,3 +31,27 @@ function smallestName(names) {
 const names = ["rahim", "robin", "rafi", "ron", "rashed"];
 const output1 = smallestName(names);
 console.log(output1);
+// Task-3:
+// Your task is to calculate the total budget required to buy electronics:
+
+//     laptop = 35000 tk
+//     tablet = 15000 tk
+//     mobile = 20000 tk
+// Write a JavaScript function named calculateElectronicsBudget that takes in the number of laptop, tablets, and mobile and returns the total money required.
+function calculateElectronicsBudget(
+  laptopQuantity,
+  tabletQuantity,
+  mobileQuantity
+) {
+  const laptop = 35000;
+  const tablet = 15000;
+  const mobile = 20000;
+
+  const totalLaptopPrice = laptop * laptopQuantity;
+  const totalTabletPrice = tablet * tabletQuantity;
+  const totalMobilePrice = mobile * mobileQuantity;
+  const totalPrice = totalLaptopPrice + totalTabletPrice + totalMobilePrice;
+  return totalPrice;
+}
+const budget = calculateElectronicsBudget(2, 0, 1);
+console.log(budget);
