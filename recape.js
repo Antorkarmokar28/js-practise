a4c6ae07048a04d089d5f3719fac4d38f7e55251
Antorkarmokar28 <antorkarmokar28@gmail.com>
2025-11-25 22:21:47 +0600

    find longest name of the friend array

--- recape.js
+++ recape.js
@@ -3,9 +3,19 @@ const numbers = [4, 10, 9, 20, 55, 101];
 let i = 0;
 let maxNumber = 0;
 while (i < numbers.length) {
-  if (maxNumber > numbers[i]) {
+  if (maxNumber < numbers[i]) {
     maxNumber = numbers[i];
   }
   i++;
 }
 console.log(maxNumber);
+
+const friends = ["Asif", "Kanon", "Bishow", "Rumon"];
+
+let bigName = "";
+for (let i = 0; i < friends.length; i++) {
+  if (friends[i].length > bigName.length) {
+    bigName = friends[i];
+  }
+}
+console.log(bigName);
